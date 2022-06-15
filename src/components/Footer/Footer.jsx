69efc37282c44bc6a6c81c3a68ba421e6c0cb9cd
Ilya Trubitcin <lionen89@yandex.ru>
2022-06-16 00:29:22 +0300

    fix/добавить бургерное меню

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,12 +7,12 @@ function Footer() {
       <div className="footer__nav">
       <p className="footer__copiright">&#169;2022</p>
       <ul className="footer__links">
-        <a href="https://practicum.yandex.ru/"
-            className="footer__link">Яндекс.Практикум</a>
-        <a href="https://github.com/Lionen89"
-        className="footer__link">Github</a>
-        <a href="https://www.facebook.com/profile.php?id=100001774543665"
-        className="footer__link">Facebook</a>
+        <li><a href="https://practicum.yandex.ru/"
+            className="footer__link">Яндекс.Практикум</a></li>
+        <li><a href="https://github.com/Lionen89"
+        className="footer__link">Github</a></li>
+        <li><a href="https://www.facebook.com/profile.php?id=100001774543665"
+        className="footer__link">Facebook</a></li>
       </ul>
       </div>
     </footer>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,18 +5,23 @@ function Navigation(props) {
   const location = useLocation();
   if (location.pathname === "/") {
     return (
-      <ul className="nav-auth">
+      <div className="nav-auth">
         <Link to="/signup" className="nav-auth__link">
           Регистрация
         </Link>
         <Link to="/signin" className="nav-auth__link">
           Войти
         </Link>
-      </ul>
+      </div>
     );
   } else {
     return (
-      <ul className="navigation">
+      <>
+      <div className="navigation__overlay"></div>
+      <div className="navigation">
+        <Link to="/" className="navigation__link-film">
+          Главная
+        </Link>
         <Link
           to="/movies"
           className={`navigation__link-film ${
@@ -44,7 +49,11 @@ function Navigation(props) {
           Аккаунт
           <div className="navigation__link_profile_profile-img"></div>
         </button>
-      </ul>
+      </div>
+      <div class="navigation__burger-menu">
+          <span className="navigation__burger-string"></span>
+        </div>
+      </>
     );
   }
 }
