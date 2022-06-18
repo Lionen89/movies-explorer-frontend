f8a30ae3aab68f2cec4bf340c2d9940a91830c6e
Ilya Trubitcin <lionen89@yandex.ru>
2022-06-18 12:43:45 +0300

    fix/исправить NavTab под список

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -2,16 +2,24 @@ import "./NavTab.css";
 
 function NavTab() {
   return (
-    <section className="navtab">
-      <a href="#about" className="nav__link">
-        О проекте
-      </a>
-      <a href="#techs" className="nav__link">
-        Технологии
-      </a>
-      <a href="#about-me" className="nav__link">
-        Студент
-      </a>
+    <section>
+      <ul className="navtab">
+        <li className="nav__link">
+          <a className="nav__link_text" href="#about">
+            О проекте
+          </a>
+        </li>
+        <li className="nav__link">
+          <a className="nav__link_text" href="#techs">
+            Технологии
+          </a>
+        </li>
+        <li className="nav__link">
+          <a className="nav__link_text" href="#about-me">
+            Студент
+          </a>
+        </li>
+      </ul>
     </section>
   );
 }
